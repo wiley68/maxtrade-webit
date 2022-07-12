ddf720faa4beacab51339863c19b19a3e69306c6
wiley68 <ilko.iv@gmail.com>
2022-07-12 14:51:23 +0300

    init laravel, breeze, vue

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>dashboard</h1>
+  <Link :href="route('logout')" method="post" as="button"> logout </Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+</script>
