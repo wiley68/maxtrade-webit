bd594786a60694a7a637ae8bfde4ac52f33f851d
wiley68 <ilko.iv@gmail.com>
2022-07-12 16:17:59 +0300

    add layout

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>layout</h1>
+  <slot></slot>
+</template>
+
+<script setup></script>
--- resources/js/appinertia.js
+++ resources/js/appinertia.js
@@ -5,17 +5,18 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
+import Layout from './Shared/Layout.vue'
 
 const appName =
   window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel'
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
-  resolve: (name) =>
-    resolvePageComponent(
-      `./Pages/${name}.vue`,
-      import.meta.glob('./Pages/**/*.vue')
-    ),
+  resolve: async (name) => {
+    let page = (await import(`./Pages/${name}.vue`)).default
+    page.layout ??= Layout
+    return page
+  },
   setup({ el, app, props, plugin }) {
     return createApp({ render: () => h(app, props) })
       .use(plugin)
@@ -24,4 +25,4 @@ createInertiaApp({
   },
 })
 
-InertiaProgress.init({ color: '#4B5563' })
+InertiaProgress.init({ color: 'red', showSpinner: true })
