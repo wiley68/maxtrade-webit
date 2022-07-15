4a07ab2ec73f8915939def1cbb46d4d7b259a2ea
wiley68 <ilko.iv@gmail.com>
2022-07-15 11:41:49 +0300

    left sidebar menu

--- resources/js/Shared/Components/LeftSidebar.vue
+++ resources/js/Shared/Components/LeftSidebar.vue
@@ -1,35 +1,15 @@
 <template>
   <div class="bg-gray-100 hidden md:flex w-48 h-full">
-    <!-- left sidebar menu -->
-    <div
-      class="bg-gray-200 w-12 border-r border-r-gray-300 flex flex-col items-center pt-2"
-    >
-      <div
-        class="text-gray-600 hover:text-gray-800 pb-3 cursor-pointer"
-        title="HTML Headers"
-      >
-        <svg class="w-8 h-8" viewBox="0 0 24 24">
-          <path
-            fill="currentColor"
-            d="M4,4H6V10H10V4H12V18H10V12H6V18H4V4M14,10V8H21V10H14M14,12H21V14H14V12Z"
-          />
-        </svg>
-      </div>
-      <div
-        class="text-gray-600 hover:text-gray-800 pb-3 cursor-pointer"
-        title="Layouts"
-      >
-        <svg class="w-8 h-8" viewBox="0 0 24 24">
-          <path
-            fill="currentColor"
-            d="M18 2H6C4.89 2 4 2.9 4 4V20C4 21.11 4.89 22 6 22H18C19.11 22 20 21.11 20 20V4C20 2.9 19.11 2 18 2M18 20H6V16H18V20M18 8H6V4H18V8Z"
-          />
-        </svg>
-      </div>
-    </div>
+    <LeftSidebarMenu></LeftSidebarMenu>
     <!-- left sidebar component -->
-    <div class="flex-grow">lsc</div>
+    <div
+      class="flex flex-col items-center flex-grow border-r border-r-gray-200 pt-2"
+    >
+      lsc
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import LeftSidebarMenu from './LeftSidebarMenu.vue'
+</script>
---
+++ resources/js/Shared/Components/LeftSidebarMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="bg-gray-200 w-12 border-r border-r-gray-300 flex flex-col items-center pt-2"
+  >
+    <div
+      class="text-gray-600 hover:text-gray-900 pb-3 cursor-pointer"
+      title="HTML Headers"
+    >
+      <svg class="w-8 h-8" viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M4,4H6V10H10V4H12V18H10V12H6V18H4V4M14,10V8H21V10H14M14,12H21V14H14V12Z"
+        />
+      </svg>
+    </div>
+    <div
+      class="text-gray-600 hover:text-gray-900 pb-3 cursor-pointer"
+      title="Layouts"
+    >
+      <svg class="w-8 h-8" viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M18 2H6C4.89 2 4 2.9 4 4V20C4 21.11 4.89 22 6 22H18C19.11 22 20 21.11 20 20V4C20 2.9 19.11 2 18 2M18 20H6V16H18V20M18 8H6V4H18V8Z"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
