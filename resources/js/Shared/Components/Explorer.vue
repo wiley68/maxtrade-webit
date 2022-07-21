c2b7c6547b3853105cb61f29d8037a5145d93a3c
wiley68 <ilko.iv@gmail.com>
2022-07-21 13:56:53 +0300

    explorer panel position

---
+++ resources/js/Shared/Components/Explorer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-red-300 hidden md:flex w-48 h-full">explorer</div>
+</template>
+
+<script setup></script>
--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -3,7 +3,7 @@
   <div class="w-full h-screen flex flex-col overflow-hidden">
     <Menu></Menu>
     <div class="flex items-center w-full flex-grow overflow-hidden">
-      <LeftSidebar class="flex-none" v-if="state.show_pallete"></LeftSidebar>
+      <Explorer class="flex-none" v-if="state.show_explorer"></Explorer>
       <div class="flex-grow flex flex-col h-full overflow-hidden">
         <WorkPanelMenu class="flex-none"></WorkPanelMenu>
         <div class="flex-grow overflow-auto">
@@ -21,13 +21,13 @@
 import { ref, provide } from 'vue'
 import { Head } from '@inertiajs/inertia-vue3'
 import Menu from './Components/Menu.vue'
-import LeftSidebar from './Components/LeftSidebar.vue'
+import Explorer from './Components/Explorer.vue'
 import RightSidebar from './Components/RightSidebar.vue'
 import WorkPanelMenu from './Components/WorkPanelMenu.vue'
 import { Tree } from '@/Components/project'
 
 const state = ref({
-  show_pallete: true,
+  show_explorer: true,
   pallete: 'head',
   show_settings: true,
   window: 'file',
