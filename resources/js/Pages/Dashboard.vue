fe4ba0559a2dac666255383381b5d5d2ed3f241d
wiley68 <ilko.iv@gmail.com>
2022-07-28 16:17:30 +0300

    project own buttons

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,9 +2,24 @@
   <div class="w-full h-full flex items-center mb-0.5">
     <div class="bg-2 w-1/2 h-full flex flex-col">
       <div
-        class="h-8 bg-gray-100 border-b border-gray-200 flex justify-center items-center text-sm font-medium"
+        class="h-10 bg-gray-100 border-b border-gray-200 flex items-center font-medium"
       >
-        Own projects - <span class="font-normal">{{ projects.length }}</span>
+        <button
+          type="button"
+          class="ml-1 inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-sky-50 hover:bg-sky-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+        >
+          New project
+        </button>
+        <div class="flex items-center flex-grow justify-center">
+          <div>Own projects</div>
+          <svg class="w-4 h-4 text-gray-400 mx-1" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z"
+            />
+          </svg>
+          <div class="font-normal">{{ projects.length }}</div>
+        </div>
       </div>
       <div class="h-full flex flex-grow">
         <div
@@ -43,10 +58,16 @@
             </div>
           </button>
         </div>
-        <div class="bg-gray-50 flex-grow flex flex-col px-1 text-sm pt-1">
-          <div v-if="Object.keys(currentProject).length !== 0">
+        <div class="bg-gray-50 flex-grow flex flex-col text-sm pt-1">
+          <div
+            v-if="Object.keys(currentProject).length !== 0"
+            class="flex-grow px-1"
+          >
             <div>
-              Name: <span class="font-medium">{{ currentProject.value }}</span>
+              Name:
+              <span class="font-medium text-sky-600">{{
+                currentProject.value
+              }}</span>
             </div>
             <div>
               Description:
@@ -60,6 +81,29 @@
             >
               project
             </div>
+          </div>
+          <div
+            v-if="Object.keys(currentProject).length !== 0"
+            class="h-10 w-full border-t border-gray-300 flex items-center p-1"
+          >
+            <button
+              type="button"
+              class="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+            >
+              Edit project
+            </button>
+            <button
+              type="button"
+              class="ml-1 inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+            >
+              Clone project
+            </button>
+            <button
+              type="button"
+              class="ml-1 inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-red-50 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+            >
+              Delete project
+            </button>
           </div>
         </div>
       </div>
