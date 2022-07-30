addc69a6c16d9e29fd8c2c7add9c64b53a5f54f5
wiley68 <ilko.iv@gmail.com>
2022-07-30 18:23:10 +0300

    menu project

--- resources/js/Shared/Components/Menu.vue
+++ resources/js/Shared/Components/Menu.vue
@@ -14,7 +14,74 @@
           <button>File</button>
           <ul>
             <li>
+              <Link
+                :href="route('dashboard')"
+                as="button"
+                class="w-full text-left"
+                >Open Dashboard</Link
+              >
+            </li>
+            <li>
               <a :href="route('index')" as="button" @click="logout()">Exit</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <button>Project</button>
+          <ul>
+            <li>
+              <Link
+                v-if="$page.component === 'Dashboard'"
+                :href="route('project.new')"
+                method="post"
+                as="button"
+                class="w-full text-left"
+                >New Project</Link
+              >
+            </li>
+            <li>
+              <Link
+                v-if="project.root.value.length !== 0"
+                :href="route('app')"
+                as="button"
+                class="w-full text-left"
+                >Edit Current Project</Link
+              >
+            </li>
+            <li>
+              <button
+                v-if="
+                  $page.component === 'App' && project.root.value.length !== 0
+                "
+                @click.stop="saveProject()"
+                type="button"
+                class="w-full text-left"
+              >
+                Save Current Project
+              </button>
+            </li>
+            <li>
+              <button
+                v-if="
+                  $page.component === 'Dashboard' &&
+                  project.root.value.length !== 0
+                "
+                @click.stop="cloneProject()"
+                type="button"
+                class="w-full text-left"
+              >
+                Clone Current Project
+              </button>
+            </li>
+            <li>
+              <button
+                v-if="project.root.value.length !== 0"
+                @click.stop="deleteProjectCheck()"
+                type="button"
+                class="w-full text-left"
+              >
+                Delete Current Project
+              </button>
             </li>
           </ul>
         </li>
@@ -115,7 +182,10 @@
     </div>
     <div class="w-1/3 flex flex-col">
       <div class="w-full text-sm font-medium text-gray-600 text-center">
-        {{ project.find('project').value }} - Maxtrade Webit
+        {{ project.find('project').value }} -
+        <span class="text-gray-400"
+          >Maxtrade Webit [{{ $page.props.auth.user.name }}]</span
+        >
       </div>
       <div class="w-full text-xs text-gray-500 text-center truncate">
         {{ project.find('project').description }}
@@ -158,17 +228,105 @@
       </button>
     </div>
   </div>
+  <ModalBlank
+    id="danger-modal"
+    :modalOpen="deleteProjectModal"
+    @close-modal="deleteProjectModal = false"
+  >
+    <div class="p-5 flex space-x-4">
+      <div
+        class="w-10 h-10 rounded-full flex items-center justify-center shrink-0 bg-red-100"
+      >
+        <svg
+          class="w-4 h-4 shrink-0 fill-current text-red-600"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1zm1-3H7V4h2v5z"
+          />
+        </svg>
+      </div>
+      <div>
+        <div class="mb-2">
+          <div class="text-lg font-semibold text-gray-800">Attention!</div>
+        </div>
+        <!-- Modal content -->
+        <div class="text-sm mb-10">
+          <div class="space-y-2">
+            <p>
+              Do you want to delete the project? The action is irreversible!
+            </p>
+          </div>
+        </div>
+        <div class="flex flex-wrap justify-end space-x-2">
+          <button
+            class="ml-1 inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+            @click.stop="deleteProjectModal = false"
+          >
+            Cancel
+          </button>
+          <button
+            class="ml-1 inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-red-50 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+            @click.stop="deleteProject()"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  </ModalBlank>
 </template>
 
 <script setup>
 import { useForm, Link } from '@inertiajs/inertia-vue3'
-import { inject } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+import { inject, ref } from 'vue'
+import ModalBlank from '@/Components/ModalBlank.vue'
+import { Tree } from '@/Components/project'
 
 const state = inject('state')
 const project = inject('project')
 const form = useForm({})
 const logout = () => {
   form.post(route('logout'))
+}
+const deleteProjectModal = ref(false)
+
+const deleteProjectCheck = () => {
+  deleteProjectModal.value = true
+}
+
+const deleteProject = () => {
+  deleteProjectModal.value = false
+  const form = useForm({
+    id: project.value.data.id,
+  })
+  form.post('/project/delete', {
+    onSuccess: () => {
+      project.value = new Tree('project', '')
+      Inertia.visit('/dashboard')
+    },
+  })
+}
+
+const saveProject = () => {
+  const form = useForm({
+    id: project.value.data.id,
+    name: project.value.data.name,
+    user_id: project.value.data.user_id,
+    created_at: project.value.data.created_at,
+    updated_at: project.value.data.updated_at,
+    type: project.value.data.type,
+    project: '{"root":' + JSON.stringify(project.value.root, null, 2) + '}',
+  })
+  form.post('/project/save')
+}
+
+const cloneProject = () => {
+  const form = useForm({
+    id: project.value.data.id,
+  })
+  form.post('/project/clone')
 }
 
 const toggleExplorer = () => {
