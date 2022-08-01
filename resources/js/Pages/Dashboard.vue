d8769903e5ffcc30e6883a592baa490f21a33010
wiley68 <ilko.iv@gmail.com>
2022-08-01 09:53:01 +0300

    new project action style

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -10,7 +10,12 @@
           as="button"
           class="ml-1 inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-sky-50 hover:bg-sky-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
         >
-          New project
+          <svg class="w-4 h-4" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z"
+            /></svg
+          ><span class="ml-1">New project</span>
         </Link>
         <div class="flex items-center flex-grow justify-center">
           <div>{{ $page.props.auth.user.name }}</div>
