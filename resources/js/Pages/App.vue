47672907dd214dcdd1a09cd7a3af9e12f3c7c1f1
wiley68 <ilko.iv@gmail.com>
2022-08-01 11:04:03 +0300

    app buttons style

--- resources/js/Pages/App.vue
+++ resources/js/Pages/App.vue
@@ -27,14 +27,28 @@
       type="button"
       class="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-sky-50 hover:bg-sky-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
     >
-      Save project
+      <div class="flex items-center">
+        <svg class="w-4 h-4" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M17 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.89 21 5 21H19C20.1 21 21 20.1 21 19V7L17 3M19 19H5V5H16.17L19 7.83V19M12 12C10.34 12 9 13.34 9 15S10.34 18 12 18 15 16.66 15 15 13.66 12 12 12M6 6H15V10H6V6Z"
+          /></svg
+        ><span class="ml-1">Save project</span>
+      </div>
     </button>
     <button
       @click.stop="deleteProjectCheck()"
       type="button"
       class="ml-1 inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-red-50 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
     >
-      Delete project
+      <div class="flex items-center">
+        <svg class="w-4 h-4" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z"
+          /></svg
+        ><span class="ml-1">Delete project</span>
+      </div>
     </button>
     <ModalBlank
       id="danger-modal"
