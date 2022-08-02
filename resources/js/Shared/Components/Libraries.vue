dad947a8acd87380909107adf44701840f599dd4
wiley68 <ilko.iv@gmail.com>
2022-08-02 15:35:40 +0300

    meta keywords

--- resources/js/Shared/Components/Libraries.vue
+++ resources/js/Shared/Components/Libraries.vue
@@ -164,6 +164,21 @@
           </svg>
           <span class="ml-1">meta.description</span>
         </button>
+        <button
+          @click.stop="addMeta('keywords')"
+          :disabled="add_metakeywords_disabled"
+          type="button"
+          :class="add_metakeywords_disabled ? 'text-gray-300' : 'text-gray-600'"
+          class="inline-flex w-full px-1 py-0.5 border border-gray-300 shadow-sm text-xs rounded bg-white hover:bg-gray-50 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+        >
+          <svg class="w-4 h-4" viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M14,12H15.5V14.82L17.94,16.23L17.19,17.53L14,15.69V12M4,2H18A2,2 0 0,1 20,4V10.1C21.24,11.36 22,13.09 22,15A7,7 0 0,1 15,22C13.09,22 11.36,21.24 10.1,20H4A2,2 0 0,1 2,18V4A2,2 0 0,1 4,2M4,15V18H8.67C8.24,17.09 8,16.07 8,15H4M4,8H10V5H4V8M18,8V5H12V8H18M4,13H8.29C8.63,11.85 9.26,10.82 10.1,10H4V13M15,10.15A4.85,4.85 0 0,0 10.15,15C10.15,17.68 12.32,19.85 15,19.85A4.85,4.85 0 0,0 19.85,15C19.85,12.32 17.68,10.15 15,10.15Z"
+            />
+          </svg>
+          <span class="ml-1">meta.keywords</span>
+        </button>
       </div>
       <div v-if="state.library === 'attributes'">
         <button
@@ -243,6 +258,14 @@ const add_metadescription_disabled = computed(() => {
     state.value.current_element === '' ||
     state.value.current_element !== 'head' ||
     project.value.find('description') !== undefined
+  )
+})
+
+const add_metakeywords_disabled = computed(() => {
+  return (
+    state.value.current_element === '' ||
+    state.value.current_element !== 'head' ||
+    project.value.find('keywords') !== undefined
   )
 })
 
@@ -340,6 +363,11 @@ const addMeta = (meta_id) => {
           case 'description':
             obj['name'] = meta_id
             obj['content'] = 'Project description ...'
+            meta.attributes = { ...meta.attributes, ...obj }
+            break
+          case 'keywords':
+            obj['name'] = meta_id
+            obj['content'] = ''
             meta.attributes = { ...meta.attributes, ...obj }
             break
           default:
