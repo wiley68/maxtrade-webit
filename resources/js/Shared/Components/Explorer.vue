c93e70518259d39214e6a60117be5fc01f1b2a87
wiley68 <ilko.iv@gmail.com>
2022-08-02 10:46:23 +0300

    add meta description

--- resources/js/Shared/Components/Explorer.vue
+++ resources/js/Shared/Components/Explorer.vue
@@ -157,6 +157,23 @@
               </svg>
               <span class="ml-1">meta.viewport</span>
             </button>
+            <button
+              @click.stop="addMeta('description')"
+              :disabled="add_metadescription_disabled"
+              type="button"
+              :class="
+                add_metadescription_disabled ? 'text-gray-300' : 'text-gray-600'
+              "
+              class="inline-flex w-full px-1 py-0.5 border border-gray-300 shadow-sm text-xs rounded bg-white hover:bg-gray-50 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            >
+              <svg class="w-4 h-4" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M14,12H15.5V14.82L17.94,16.23L17.19,17.53L14,15.69V12M4,2H18A2,2 0 0,1 20,4V10.1C21.24,11.36 22,13.09 22,15A7,7 0 0,1 15,22C13.09,22 11.36,21.24 10.1,20H4A2,2 0 0,1 2,18V4A2,2 0 0,1 4,2M4,15V18H8.67C8.24,17.09 8,16.07 8,15H4M4,8H10V5H4V8M18,8V5H12V8H18M4,13H8.29C8.63,11.85 9.26,10.82 10.1,10H4V13M15,10.15A4.85,4.85 0 0,0 10.15,15C10.15,17.68 12.32,19.85 15,19.85A4.85,4.85 0 0,0 19.85,15C19.85,12.32 17.68,10.15 15,10.15Z"
+                />
+              </svg>
+              <span class="ml-1">meta.description</span>
+            </button>
           </div>
           <div v-if="state.library === 'attributes'">
             <button
@@ -284,6 +301,13 @@ const add_metaviewport_disabled = computed(() => {
     state.value.current_element === '' ||
     state.value.current_element !== 'head' ||
     project.value.find('viewport') !== undefined
+  )
+})
+const add_metadescription_disabled = computed(() => {
+  return (
+    state.value.current_element === '' ||
+    state.value.current_element !== 'head' ||
+    project.value.find('description') !== undefined
   )
 })
 const add_attribute_disabled = computed(() => {
@@ -426,6 +450,11 @@ const addMeta = (meta_id) => {
           case 'viewport':
             obj['name'] = meta_id
             obj['content'] = 'width=device-width, initial-scale=1'
+            meta.attributes = { ...meta.attributes, ...obj }
+            break
+          case 'description':
+            obj['name'] = meta_id
+            obj['content'] = 'Project description ...'
             meta.attributes = { ...meta.attributes, ...obj }
             break
           default:
